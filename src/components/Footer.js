f1c0418040f13a9a2f308adb2ecc622b4e81d73b
Eliedson <especialista1979@gmail.com>
2022-12-30 20:24:57 -0300

    novo_projetos :rocket::octocat:

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>Desenvolvido por Eliedson Gomes.</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
